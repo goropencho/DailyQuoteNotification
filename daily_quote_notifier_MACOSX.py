88b85c2035d11d4f5eef52657839e282acc109d3
Gaurav Panchal <spydaddy@Gauravs-iMac.local>
2021-08-11 19:35:50 +0530

    Silly Changes

--- daily_quote_notifier_MACOSX.py
+++ daily_quote_notifier_MACOSX.py
@@ -4,9 +4,9 @@ from pync import Notifier
 import random
 
 url = requests.get("https://www.brainyquote.com/topics/daily-quotes").text
-
 soup = BeautifulSoup(url, 'lxml')
 
-tag = soup.find('div',class_="grid-item qb clearfix bqQt b-qt-lg",id=f"pos_1_{random.randint(1,2)}").a.text
+tag = soup.find('div',id=f"pos_1_{random.randint(1,2)}",class_="grid-item qb clearfix bqQt").a.text
+tag = tag.lstrip().rstrip()
 
 Notifier.notify(tag, title="Daily Quote")
